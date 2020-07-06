3b9d6794da13722688d21297ddc8ac3ef55b5a4e
Alex Foreman <foreman.alexander@gmail.com>
2020-07-06 18:00:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,6 +29,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  new_value = [matrix][row][column]
-  return new_value
+  matrix[row][column] = new_value 
+  return matrix[row][column]
 end
